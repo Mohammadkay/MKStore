952f8371dee67e3fa68949c79dc1568a256f697c
[Mohammadkay] <[mohamd.al.alkayyali@gmail.com]>
2023-09-26 21:56:32 +0300

    finshess

--- Dashboard/src/Component/Shops/Category/AddCategory.jsx
+++ Dashboard/src/Component/Shops/Category/AddCategory.jsx
@@ -59,17 +59,17 @@ export default function AddCategory() {
                 <div className="card mb-4">
                     <div className="card-header">
                         <svg className="svg-inline--fa fa-table me-1" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="table" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg><path fill="currentColor" d="M448 32C483.3 32 512 60.65 512 96V416C512 451.3 483.3 480 448 480H64C28.65 480 0 451.3 0 416V96C0 60.65 28.65 32 64 32H448zM224 256V160H64V256H224zM64 320V416H224V320H64zM288 416H448V320H288V416zM448 256V160H288V256H448z" /></svg>{/* <i className="fas fa-table me-1"></i> Font Awesome fontawesome.com */}
-                        Add Product
+                        Add New Category
                 </div>
                 {/* start form */}
                 <form onSubmit={addProduct}>
                     <div className="row p-3">
                         <div className="col">
-                         <label htmlFor="">Product Name</label>
-                        <input type="text" className="form-control" placeholder="Product Name" ref={name}/>
+                         <label htmlFor="">Category Name</label>
+                        <input type="text" className="form-control" placeholder="Category Name" ref={name}/>
                     </div>
                     <div className="col">
-                         <label htmlFor="">Product Name</label>
+                         <label htmlFor="">Category Image</label>
                         <input type="file" className="form-control"  onChange={(e)=>setImage(e.target.files[0])}/>
                     </div>
                 </div>
